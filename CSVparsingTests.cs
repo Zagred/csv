6ab915aa6378614846543198900733182019443e
paco <74721116+Zagred@users.noreply.github.com>
2023-09-04 12:18:05 +0300

    Add files via upload

---
+++ CSVparsingTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSVLibrary;
+
+namespace Library.Tests
+{
+    [TestClass()]
+    public class CSVparsingTests
+    {
+        [TestMethod()]
+        public void Test()
+        {
+                using (var reader = new StreamReader(File.OpenRead(@"C:\Users\paco\Desktop\csv-main\document.csv")))
+                {
+                    if (CSVparsing.FileParsing(reader) == null)
+                    {
+                        Assert.Fail();
+                    }
+                }
+        }   
+    }
+}
